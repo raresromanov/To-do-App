830793ea4049132c055fe96954b19c8ffd02d4ce
Rares Mihai Romanov <rares.romanov@netcentric.biz>
2022-09-12 09:39:53 +0300

    added delete and edit func

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', ThingsList.as_view(), name='home'),
     path('create/', CreateThing.as_view(), name='create_thing'),
     path('results/', SearchView.as_view(), name='search'),
+    path('delete/<int:pk>', DeleteThingView.as_view(), name='delete_thing'),
+    path('edit/<int:pk>', EditThingView.as_view(), name='edit_thing'),
 ]
--- todoApp/views.py
+++ todoApp/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView
 from todoApp.models import Thing
+from django.db.models import Q
 
 
 # Create your views here.
@@ -14,23 +15,35 @@ class ThingsList(ListView):
     template_name = 'home.html'
     context_object_name = 'thing'
 
+
 class CreateThing(CreateView):
     model = Thing
     template_name = 'create_thing.html'
     fields = '__all__'
     success_url = reverse_lazy('home')
 
+
 class SearchView(ListView):
     model = Thing
-    template_name = 'thing_search.html'
-    context_object_name = 'all_search_results'
+    template_name = "thing_search.html"
 
-    def get_queryset(self):
-        result = super(SearchView, self).get_queryset()
-        query = self.request.GET.get('search')
-        if query:
-            postresult = Thing.objects.filter(name__contains=query)
-            result = postresult
-        else:
-            result = None
-        return result
+    def get_queryset(self):  # new
+        query = self.request.GET.get("search")
+        object_list = Thing.objects.filter(
+            Q(name__icontains=query)
+        )
+        return object_list
+
+class DeleteThingView(DeleteView):
+    model = Thing
+    template_name = "delete_thing.html"
+    context_object_name = "thing"
+    success_url = reverse_lazy('home')
+
+class EditThingView(UpdateView):
+    model = Thing
+    template_name = "update_thing.html"
+    context_object_name = 'thing'
+    fields = '__all__'
+    success_url = reverse_lazy('home')
+
